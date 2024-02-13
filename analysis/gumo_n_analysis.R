7edf6901aad0666225d342b1d95efb5f14a6dff0
Enrique Garcia <enriqgar@ttu.edu>
2024-02-13 15:54:38 -0600

    added in gumo chemistry data created a r script for analysis

---
+++ analysis/gumo_n_analysis.R
@@ -0,0 +1,6 @@
+# gumo_n_analysis.R
+## script for analyzing the gumo data for changes in leaf n with elevation
+
+## read in data
+gumo_n_data <- read.csv("../data/gumo_n_data.csv")
+head(gumo_n_data)
