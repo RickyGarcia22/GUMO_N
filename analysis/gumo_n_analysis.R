8fc63d410d7eb31c48fa62203799705abb411cc8
Enrique Garcia <enriqgar@ttu.edu>
2024-02-13 16:17:58 -0600

    Added to do list

--- analysis/gumo_n_analysis.R
+++ analysis/gumo_n_analysis.R
@@ -4,3 +4,15 @@
 ## read in data
 gumo_n_data <- read.csv("../data/gumo_n_data.csv")
 head(gumo_n_data)
+
+## examine nitrogen-elevation trends
+
+### plot leaf nitrogen against elevation
+# plot(n_weight_p ~ )
+
+
+#####################
+#### to do
+#####################
+# feb 13, 2024: get elevation data into main dataset, plot N as a function of elevation, 
+# fit linear model of n as a function of elevation
